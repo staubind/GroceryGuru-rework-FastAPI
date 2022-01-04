675841c0e4360dfc6792c2b47a92e347266a3709
Dan St. Aubin <staubindj@gmail.com>
2022-01-03 21:45:56 -0600

    need to set up the database next

--- server/routes/user_router.py
+++ server/routes/user_router.py
@@ -1,4 +1,5 @@
 
 # probably you'll want to look at https://fastapi.tiangolo.com/tutorial/cookie-params/
 # when it comes to users - you'll need to use cookies and JWTs, I believe
+# also username and password need to be passed as form data, so: https://fastapi.tiangolo.com/tutorial/request-forms/
 
