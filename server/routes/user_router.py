507faf8d79345b76edc4f30c02d85b2d6a95d71d
Dan St. Aubin <staubindj@gmail.com>
2022-01-22 23:22:43 -0600

    set up the dependency for auth

--- server/routes/user_router.py
+++ server/routes/user_router.py
@@ -3,3 +3,4 @@
 # when it comes to users - you'll need to use cookies and JWTs, I believe
 # also username and password need to be passed as form data, so: https://fastapi.tiangolo.com/tutorial/request-forms/
 
+
